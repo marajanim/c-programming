61fd92a5090e49f3e528c60290deb4c41ac34c68
marajanim <animm914@gmail.com>
2023-04-07 03:42:56 +0600

    sum

---
+++ Setup/N_times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    int N = 0;
+    for (i;i <= N <=1000;i=i++) 
+    {
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
---
+++ Setup/practice-problem-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int A=0, B=0;
+    int sum = A+B;
+    scanf("%d %d", &A, &B);
+    printf("%d",A+B);
+    return 0;
+};
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i = 0;
+//     while (i = 0) {
+//         printf("Hello\n");
+//     }
+//     return 0;
+// }
